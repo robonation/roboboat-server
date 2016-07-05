dedc7315dadd469df0baaa98a406c001c5fc5ef4
Felix Pageau <felix.pageau@gmail.com>
2016-07-05 08:58:57 -0400

    tuesday setup

--- src/main/java/com/felixpageau/roboboat/mission/AdminResourceConfig.java
+++ src/main/java/com/felixpageau/roboboat/mission/AdminResourceConfig.java
@@ -57,16 +57,16 @@ public class AdminResourceConfig extends CompetitionResourceConfig {
       new CompetitionDay(LocalDateTime.of(2016, 7, 9, 8, 0), LocalDateTime.of(2016, 7, 9, 18, 0)), // Sa
       new CompetitionDay(LocalDateTime.of(2016, 7, 10, 8, 0), LocalDateTime.of(2016, 7, 10, 18, 0)) // Su
       );
-  private static final List<TeamCode> TEAMS = TeamCode.of("AUVSI", "DBH", "EEPIS", "ERAU", "FAU", "GIT", "NCKU", "ODUSM", "ODUBB", "TUCE", "CUA", "UCF", "UF",
-      "UOFM", "ULSAN", "UWF", "VU");
-  private static final List<DockingBay> dockingBaysA = ImmutableList.of(new DockingBay(Symbol.circle, SymbolColor.black), new DockingBay(Symbol.circle,
-      SymbolColor.green), new DockingBay(Symbol.triangle, SymbolColor.green));
-  private static final List<DockingBay> dockingBaysB = ImmutableList.of(new DockingBay(Symbol.triangle, SymbolColor.black), new DockingBay(Symbol.circle,
-      SymbolColor.blue), new DockingBay(Symbol.cruciform, SymbolColor.green));
+  private static final List<TeamCode> TEAMS = TeamCode.of("AUVSI", "DBH", "ERAU", "FAU", "GIT", "ITSN", "ODUSM", "ODUBF", "SRM", "USNA", "AMVUI", "UOFM",
+      "ULSAN", "UWF");
+  private static final List<DockingBay> dockingBaysA = ImmutableList.of(new DockingBay(Symbol.triangle, SymbolColor.red), new DockingBay(Symbol.triangle,
+      SymbolColor.black), new DockingBay(Symbol.cruciform, SymbolColor.black));
+  private static final List<DockingBay> dockingBaysB = ImmutableList.of(new DockingBay(Symbol.circle, SymbolColor.blue), new DockingBay(Symbol.cruciform,
+      SymbolColor.red), new DockingBay(Symbol.cruciform, SymbolColor.blue));
   private static final List<DockingBay> dockingBaysOpenTest = ImmutableList.of(new DockingBay(Symbol.triangle, SymbolColor.red), new DockingBay(
       Symbol.cruciform, SymbolColor.black), new DockingBay(Symbol.cruciform, SymbolColor.blue));
-  private static final List<Pinger> courseAPingers = ImmutableList.of(new Pinger(BuoyColor.black), new Pinger(BuoyColor.green), new Pinger(BuoyColor.red));
-  private static final List<Pinger> courseBPingers = ImmutableList.of(new Pinger(BuoyColor.green), new Pinger(BuoyColor.red), new Pinger(BuoyColor.black));
+  private static final List<Pinger> courseAPingers = ImmutableList.of(new Pinger(BuoyColor.green), new Pinger(BuoyColor.red), new Pinger(BuoyColor.black), new Pinger(BuoyColor.yellow));
+  private static final List<Pinger> courseBPingers = ImmutableList.of(new Pinger(BuoyColor.green), new Pinger(BuoyColor.red), new Pinger(BuoyColor.black), new Pinger(BuoyColor.yellow));
   private static final List<Pinger> openTestPingers = ImmutableList.of(new Pinger(BuoyColor.black), new Pinger(BuoyColor.blue), new Pinger(BuoyColor.red),
       new Pinger(BuoyColor.yellow), new Pinger(BuoyColor.green));
   public static final Map<Course, CourseLayout> COURSE_LAYOUT_MAP;
@@ -75,9 +75,9 @@ public class AdminResourceConfig extends CompetitionResourceConfig {
 
   static {
     try {
-      COURSE_LAYOUT_MAP = ImmutableMap.of(Course.courseA, new CourseLayout(Course.courseA, courseAPingers, dockingBaysA, new URL("http://192.168.1.5:5000"),
+      COURSE_LAYOUT_MAP = ImmutableMap.of(Course.courseA, new CourseLayout(Course.courseA, courseAPingers, dockingBaysA, new URL("http://192.168.1.5:4000"),
           new URL("http://192.168.1.7:5000")), Course.courseB, new CourseLayout(Course.courseB, courseBPingers, dockingBaysB,
-          new URL("http://192.168.1.6:5000"), new URL("http://192.168.1.8:5000")), Course.openTest, new CourseLayout(Course.openTest, openTestPingers,
+          new URL("http://192.168.1.6:4000"), new URL("http://192.168.1.8:5000")), Course.openTest, new CourseLayout(Course.openTest, openTestPingers,
           dockingBaysOpenTest, new URL("http://127.0.0.1:5000"), new URL("http://127.0.0.1:5000")));
     } catch (MalformedURLException e) {
       throw new RuntimeException(e);
